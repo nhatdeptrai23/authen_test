03e66b155199711100b34cff5315ac08cb257342
nhatdeptrai23 <japnh24>
2022-09-04 13:29:44 +0700

    Add part 3

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
     password: String,
 });
 
-const secret = "This is our litter secret";
+
 userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
